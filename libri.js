27cf1b8c5075db1e63d27c0b041d48d42c1cd5dd
Federico Scutariu <82754015+federicoscutariu@users.noreply.github.com>
2024-11-21 15:22:19 +0100

    Adjust home page

---
+++ libri.js
@@ -0,0 +1,259 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const libri = [
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+    {
+      titolo: "Swag Negro",
+      autore: "Bello Figo Kiaro",
+      pubblicazione: "30 aprile 2018",
+      copertina:
+        "https://m.media-amazon.com/images/I/91jBfBu+tYL._UF1000,1000_QL80_.jpg",
+      link: "https://www.amazon.it/Swag-negro-Bello-Figo-ebook/dp/B07CJ68V78",
+    },
+  ];
+
+  const recensioni = [
+    {
+      titolo: "non pago affitto billone",
+      contenuto: "Questo libro ti svolta la vita kiaro billone, compra billone martelli come un coccodrillo billone",
+      stelle: 3,
+      autore:
+        "https://i.scdn.co/image/ab6761610000e5ebd5211851b090722c7e229c68",
+    },
+  ];
+
+  const sliderTrack = document.getElementById("sliderTrack");
+
+  const creaLibroHTML = (libro) => {
+    const index = libri.indexOf(libro);
+    const recensione = recensioni[0];
+    return `
+      <div class="slide">
+        <div class="book">
+          <div class="cover">
+            <img src="${libro.copertina}" alt="${libro.titolo}" width="150" />
+          </div>
+          <div class="info">
+            <span class="book-title">${libro.titolo}</span>
+            <div class="book-other-info">
+              <span class="book-author-label">Autore:</span>
+              <span class="book-author">${libro.autore}</span>
+              <span class="book-published-label">Data di pubblicazione:</span>
+              <span class="book-published">${libro.pubblicazione}</span>
+              <span class="book-link-label">Acquista ora:</span>
+              <a href="${libro.link}" target="_blank" class="book-link">Amazon</a>
+            </div>
+          </div>
+        </div>
+        <div class="review">
+            <div class="author"><img src="${recensione.autore}" width="50" height="50" class="author-img"></img></div>
+            <div class="review-info">
+                <div class="review-top">
+                    <div class="rating"></div>
+                    <span class="review-title">${recensione.titolo}</span>
+                </div>
+                <span class="review-content">${recensione.contenuto}</span>
+            </div>
+        </div>
+      </div>
+    `;
+  };
+
+  libri.forEach((libro) => {
+    const index = libri.indexOf(libro);
+    const recensione = recensioni[0];
+    sliderTrack.innerHTML += creaLibroHTML(libro);
+
+    const ratings = Array.from(document.querySelectorAll(".rating"));
+
+    for (let i = 0; i < recensione.stelle; i++) {
+      const star = document.createElement("img");
+      star.classList.add("star");
+      star.src =
+        "https://i.postimg.cc/L88MYQT7/png-clipart-star-star-removebg-preview.png";
+      ratings[index].append(star);
+    }
+  });
+});
